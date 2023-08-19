3fbafeb278cda15a61f2173d0659223b44800ce3
LuiggiDevOp <luiggi0906@gmail.com>
2023-08-18 20:43:05 -0400

    Proyecto final v1.3

---
+++ src/proyectofinal/Metodos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author luiggi paredes
+ */
+public class Metodos {
+    public double multiplicar(double v1, double v2){
+      return v1*v2;
+  }   
+}
